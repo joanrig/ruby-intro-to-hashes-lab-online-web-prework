492dfbb451e203ffe85a4067a6a5ee0c58d28c43
Joan in DC <joanrigdon@gmail.com>
2018-12-22 22:42:38 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -12,10 +12,13 @@ def monopoly
 end
 
 def monopoly_with_second_tier
-	monopoly = {railroads: {
-	  pieces: 4},
-	}
-	  
+	monopoly = 
+	{railroads: 
+	  {
+	  pieces: 4,
+	  names: {},
+	  rent in dollars: {}
+	  }
 	}
 	
 end
