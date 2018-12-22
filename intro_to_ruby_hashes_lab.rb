4464bfd92f1cc19b8b2fdc3a68e0fd1d45a7c364
Joan in DC <joanrigdon@gmail.com>
2018-12-22 22:47:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -28,8 +28,8 @@ def monopoly_with_third_tier
 	  {
 	  :pieces => 4,
 	  :names => {
-	    :reading_railroad => {}
-	    :pennsylvania_raiload
+	    :reading_railroad => {},
+	    :pennsylvania_raiload => {},
 	  },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
